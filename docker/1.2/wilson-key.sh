79c231b4209c5473e1b3cdae90c724b6b3302bd7
Carsten Kuenne <Carsten.kuenne@mpi-bn.mpg.de>
2018-01-09 17:23:43 +0100

    Update wilson-key.sh

--- docker/1.2/wilson-key.sh
+++ docker/1.2/wilson-key.sh
@@ -14,13 +14,19 @@
 #docker run --name='sav1' -d -p 50000:3838 -v '/mnt/agnerds/docker/projects/sav1':'/srv/shiny-server/data' -e CONTAINERKEY=sav1 loosolab/wilson:1.2"
 #-> http://<IP>:50000/sav1
 
-
 conf="/etc/shiny-server/shiny-server.conf"
 
 #create backup of original shiny-server.conf
 if [[ ! -f "${conf}.old" ]]; then
 	cp ${conf} ${conf}.old
 fi
+
+#add parameter(s) at top of .conf
+param="preserve_logs true;"
+sed -i "1i $param" ${conf}
+
+param="sanitize_errors off;"
+sed -i "1i $param" ${conf}
 
 #if environment variable given -> add key to url
 if [[ ! -z "$CONTAINERKEY" ]]; then
@@ -30,4 +36,4 @@ if [[ ! -z "$CONTAINERKEY" ]]; then
 fi
 
 #start shiny server
-shiny-server
+shiny-server >/var/log/shiny-server.log
